1bcd8885ecd4d9d9de01c64fac7646274e5c49b8
Agus Vama <canongoo72@gmail.com>
2018-07-30 14:56:03 -0500

    little refactor for send two values by serial port

--- anitaSketch/anitaSketch.ino
+++ anitaSketch/anitaSketch.ino
@@ -3,6 +3,8 @@ int trigger = 3;
 int echo2 = 6;
 int trigger2 = 7;
 double velocidadSonido = 0.0343;
+double v = 0.0;
+double r = 0.0;
 
 void setup() {
   Serial.begin(9600);
@@ -17,19 +19,24 @@ void loop() {
   delayMicroseconds(10);
   digitalWrite(trigger, LOW);
 
-  Serial.print("distancia izquierda: ");
+  //Serial.print("distancia izquierda: ");
   //se divide entre dos porque el pulseIn retorna el tiempo de ida y vuelta
-  Serial.print( (velocidadSonido * pulseIn(echo, HIGH)) / 2);
-  Serial.println(" cm");
+  //Serial.print( (velocidadSonido * pulseIn(echo, HIGH)) / 2);
+  
+  //Serial.println(" cm");
+  v = velocidadSonido * pulseIn(echo, HIGH) / 2;
 
   digitalWrite(trigger2, HIGH);
   delayMicroseconds(10);
   digitalWrite(trigger2, LOW);
 
-  Serial.print("distancia derecha: ");
+  //Serial.print("distancia derecha: ");
   //se divide entre dos porque el pulseIn retorna el tiempo de ida y vuelta
-  Serial.print( (velocidadSonido * pulseIn(echo2, HIGH)) / 2);
-  Serial.println(" cm");
-  
+  //Serial.print( (velocidadSonido * pulseIn(echo2, HIGH)) / 2);
+  //Serial.println(" cm");
+  r = velocidadSonido * pulseIn(echo2, HIGH) / 2;
+  Serial.print(v);
+  Serial.print(" ");
+  Serial.println(r);
   delay(1000);
 }
