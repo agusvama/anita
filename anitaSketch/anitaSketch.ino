75e4541c9abb7a6b0489b327dd54de17ed452aeb
Agus Vama <canongoo72@gmail.com>
2018-07-30 13:36:52 -0500

    changing sensor, now we area using HC-SR04 (ultrasonic)

--- anitaSketch/anitaSketch.ino
+++ anitaSketch/anitaSketch.ino
@@ -1,10 +1,35 @@
+int echo = 2;
+int trigger = 3;
+int echo2 = 6;
+int trigger2 = 7;
+double velocidadSonido = 0.0343;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  pinMode(echo, INPUT);  //echo
+  pinMode(trigger, OUTPUT); //trigger
+  pinMode(echo2, INPUT);
+  pinMode(trigger2, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println(analogRead(1));
+  digitalWrite(trigger, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger, LOW);
+
+  Serial.print("distancia izquierda: ");
+  //se divide entre dos porque el pulseIn retorna el tiempo de ida y vuelta
+  Serial.print( (velocidadSonido * pulseIn(echo, HIGH)) / 2);
+  Serial.println(" cm");
+
+  digitalWrite(trigger2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigger2, LOW);
+
+  Serial.print("distancia derecha: ");
+  //se divide entre dos porque el pulseIn retorna el tiempo de ida y vuelta
+  Serial.print( (velocidadSonido * pulseIn(echo2, HIGH)) / 2);
+  Serial.println(" cm");
+  
   delay(1000);
 }
