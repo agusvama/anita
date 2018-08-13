cf024b004adc91b7591bb5d446066f3d6f92e8a1
Agus Vama <canongoo72@gmail.com>
2018-08-13 03:18:52 -0500

    adding a function to select destiny for data file

--- crearArchivo.m
+++ crearArchivo.m
@@ -1,12 +1,21 @@
 function [ nombreArchivo ] = crearArchivo( fecha )
-  nombreArchivo = ...
-  strcat('projects/', int2str(fecha(1)), '-', ... %año
-                      int2str(fecha(2)), '-', ... %mes
-                      int2str(fecha(3)), '@', ... %dia
-                      int2str(fecha(4)), '_', ... %hora
-                      int2str(fecha(5)), '_', ... %minuto
-                      int2str(fecha(6)),      ... %segundo
-                      '.csv'                  ... %formato
-        );
+  %abre un cuadro de diálogo y usuario selecciona ruta para guardar su PDF
+  [file, path] = uiputfile('*.csv', 'Guardar datos como...');
+  %uiputfile te devuelve 2 variables
+  % file es el nombre del archivo
+  % path es donde se va guardar
+  if(~file)
+    warndlg('introduzca un nombre de archivo')
+    return;
+  end
+    nombreArchivo = strcat(path, file,        '_', ...
+                           int2str(fecha(1)), '-', ... %año
+                           int2str(fecha(2)), '-', ... %mes
+                           int2str(fecha(3)), '@', ... %dia
+                           int2str(fecha(4)),  '_', ... %hora
+                           int2str(fecha(5)), '_', ... %minuto
+                           int2str(fecha(6)),      ... %segundo
+                           '.csv'                  ... %formato
+          );
 end
 
